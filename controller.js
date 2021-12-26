75acd9ac9819f5e4ad154b7ad67cb24bdd25771a
Meirlianah Nurita Sari Mnanu <meirli.nurita11@gmail.com>
2021-12-26 16:05:21 +0700

    Berhasil tambah data

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ exports.index = function(req, res) {
 
 //menampilkan semua data mahasiswa
 exports.tampilsemuamahasiswa = function(req, res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
         if (error) {
             console.log(error);
         } else {
@@ -27,6 +27,22 @@ exports.tampilberdasarkanid = function(req, res) {
                 console.log(error);
             } else {
                 response.ok(rows, res);
+            }
+        });
+};
+
+//menambahkan data mahasiswa
+exports.tambahMahasiswa = function(req, res) {
+    var nim_mahasiswa = req.body.nim_mahasiswa;
+    var nama_mahasiswa = req.body.nama_mahasiswa;
+    var jurusan_mahasiswa = req.body.jurusan_mahasiswa;
+
+    connection.query('INSERT INTO mahasiswa (nim_mahasiswa, nama_mahasiswa, jurusan_mahasiswa) VALUES(?,?,?)', [nim_mahasiswa, nama_mahasiswa, jurusan_mahasiswa],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil menambahkan data!", res)
             }
         });
 };
